0f3cf70289ea644545eb6004ccb8e72a14158e69
HuaSong1116 <hsong321@qq.com>
2020-11-09 22:49:57 +0800

    tanji

--- src/main/java/cn/tycoding/action/test.java
+++ src/main/java/cn/tycoding/action/test.java
@@ -3,5 +3,6 @@ package cn.tycoding.action;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("tanji hello world");
     }
 }
